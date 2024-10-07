583673fc1822afab3755e7e9a2c9315ae67b8d99
brayanLopez <balg.lg55@gmail.com>
2024-10-06 22:05:09 -0500

    REMOVE unnecesary code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,5 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
-import { LabsComponent } from './pages/labs/labs.component';
 
-export const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'labs',
-    component: LabsComponent,
-  },
-];
+export const routes: Routes = [{ path: '', component: HomeComponent }];
